22c8ffad4d96b535ead1d5e105091341ebbcc17d
EgorTester <egorv57225@gmail.com>
2025-06-09 20:47:31 +0300

    Add files via upload

---
+++ 2_hw.py
@@ -0,0 +1,29 @@
+def task_1():
+
+    my_int = 42
+    my_float = 3.14
+    my_str = "Hello, Python!"
+    my_list = [1, 2, 3, 4, 5]
+    my_bool = True
+
+    return (type(my_int), type(my_float), type(my_str), type(my_list), type(my_bool))
+
+print(task_1())
+
+task_1()
+
+
+def task_2():
+ a = [1, 2, 3, 5, 8, 13, 21]
+ first_three = a[:3]
+ print(first_three)
+
+ return first_three
+result = task_2()
+
+
+def task_3\
+(number: int):
+ return number ** 2
+
+print(task_3(8))
